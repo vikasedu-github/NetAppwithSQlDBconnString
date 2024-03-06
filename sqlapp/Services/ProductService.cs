430147713b6e3ef363ca8fdb90b88d334c82f65f
vikasedu-github <vikas22.edu@gmail.com>
2024-03-06 16:35:51 +0530

    Pushing to Git

---
+++ sqlapp/Services/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using sqlapp.Models;
+using System.Data.SqlClient;
+
+namespace sqlapp.Services
+{
+
+    // This service will interact with our Product data in the SQL database
+    public class ProductService : IProductService
+    {
+        private readonly IConfiguration _configuration;
+        public ProductService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private SqlConnection GetConnection()
+        {
+            string connectionString = "Server=tcp:appserver6000.database.windows.net,1433;Initial Catalog=appdb;Persist Security Info=False;User ID=sqlusr;Password=Azure@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            return new SqlConnection(connectionString);
+        }
+        public List<Product> GetProducts()
+        {
+            List<Product> _product_lst = new List<Product>();
+            string _statement = "SELECT ProductID,ProductName,Quantity from Products";
+            SqlConnection _connection = GetConnection();
+
+            _connection.Open();
+
+            SqlCommand _sqlcommand = new SqlCommand(_statement, _connection);
+
+            using (SqlDataReader _reader = _sqlcommand.ExecuteReader())
+            {
+                while (_reader.Read())
+                {
+                    Product _product = new Product()
+                    {
+                        ProductID = _reader.GetInt32(0),
+                        ProductName = _reader.GetString(1),
+                        Quantity = _reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(_product);
+                }
+            }
+            _connection.Close();
+            return _product_lst;
+        }
+
+    }
+}
+
